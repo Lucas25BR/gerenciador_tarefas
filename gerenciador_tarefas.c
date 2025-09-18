e9ae3a8d621896237aa977b89282d74c8f21990a
Lucas25BR <lucas.santos25@ufmt.br>
2025-09-18 11:03:59 -0400

    Add files via upload

--- gerenciador_tarefas.c
+++ gerenciador_tarefas.c
@@ -1,7 +1,7 @@
 /*
 ##################################################
 Alunos: Lucas Viana, João Galhard, Kevin
-Versão: 
+Versão:
 ##################################################
 */
 
@@ -13,7 +13,7 @@ Versão:
 
 int top_id = 0;
 
-listas_nome[LISTA_LEN];
+listas_nome[LISTAS_LEN];
 
 int tarefas_id[TAREFAS_LEN];
 int tarefas_prioridade[TAREFAS_LEN];
@@ -22,73 +22,122 @@ int tarefas_lista[TAREFAS_LEN];
 char tarefas_nome[TAREFAS_LEN][STR_MAX_LEN];
 char tarefas_data[TAREFAS_LEN][STR_MAX_LEN];
 
+void exibir_tarefas();
+void adicionar_tarefa();
+void gerenciar_tarefas();
+void gerenciar_listas();
+void filtros();
 
-int main() {
+int main()
+{
 
-    //MENU
-    do {
-        int sel[3];
+    // MENU
+    int op;
+    do
+    {
+        exibir_tarefas();
 
-        //EXIBIR LISTA DE TAREFAS
-        printf("TAREFAS: \n");
-        for (int id = 0; id < top_id; i++) {
-            printf("%d - %s\n", id, tarefas_nome[id]);
-        }
-        //MENU DE OPÇÕES
-        
-        switch (sel[0]) {
-        //SAIR
-            case 0:
+        // MENU DE OPÇÕES
+        switch (op)
+        {
+            // SAIR
+        case 0:
             break;
-            case 1:
-                //GERENCIAR TAREFAS
-                switch (sel[1]) {
-                    //MENU INCIAL
-                    case 0:
-                    break;
-                    //CRIAR NOVA TAREFA
-                    case 1:
-                        switch (sel[2])
-                        {
-                            //MENU INICIAL
-                            case 0:
-                            break;
-                    
-                            default:
-                            break;
-                        }
-                    break;
-                    default:
-                    break;        
-                }
-            break;
-            case 2:
-                //GERENCIAR LISTAS
-                switch (sel[1]) {
-                //MENU INICIAL
-                    case 0:        
-                    break;
+        case 1:
 
-                    default:
-                    break;
-                }
             break;
-            case 3:
-                //FILTROS
-                switch (sel[1]) {
-                //MENU INICIAL
-                    case 0:            
-                    break;                    
- 
-                    default:
-                    break;
-                }
+        case 2:
+
             break;
-                
-            default:
+        case 3:
+
+            break;
+
+        default:
             break;
         }
-        
-    } while(sel[0]);
 
-return 0;
+    } while (op);
+    return 0;
+}
+
+void exibir_tarefas()
+{
+    printf("TAREFAS: \n");
+    for (int id = 0; id < top_id; id++)
+    {
+        printf("%d - %s\n", id, tarefas_nome[id]);
+    }
+
+    return;
+}
+
+void adicionar_tarefa()
+{
+    int op;
+
+    switch (op)
+    {
+    // MENU INICIAL
+    case 0:
+        break;
+
+    default:
+        break;
+    }
+
+    return;
+}
+void gerenciar_tarefas()
+{
+    int op;
+
+    switch (op)
+    {
+    // MENU INCIAL
+    case 0:
+        break;
+
+    case 1:
+
+        break;
+    default:
+        break;
+    }
+
+    return;
+}
+
+void gerenciar_listas()
+{
+    int op;
+
+    switch (op)
+    {
+    // MENU INICIAL
+    case 0:
+        break;
+
+    default:
+        break;
+    }
+
+    return;
+}
+
+void filtros()
+{
+    int op;
+
+    switch (op)
+    {
+        // MENU INICIAL
+    case 0:
+        break;
+
+    default:
+        break;
+    }
+
+    return;
+}
