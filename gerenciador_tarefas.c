e4c38e566b41f7303ca7dba5839f84abea6a7cb6
Lucas25BR <lucas.santos25@ufmt.br>
2025-09-18 12:00:00 -0400

    Add files via upload

--- gerenciador_tarefas.c
+++ gerenciador_tarefas.c
@@ -11,46 +11,59 @@ Versão:
 #define TAREFAS_LEN 0x40
 #define LISTAS_LEN 0x40
 
-int top_id = 0;
+enum{
+    EXCLUIDO,
+    EM_ANDAMENTO,
+    CONCLUIDO
+};
 
-listas_nome[LISTAS_LEN];
 
+int listas_top_id = 0;
+char listas_nome[LISTAS_LEN][STR_MAX_LEN];
+
+int tarefas_top_id = 0;
 int tarefas_id[TAREFAS_LEN];
 int tarefas_prioridade[TAREFAS_LEN];
-int tarefas_lista[TAREFAS_LEN];
-
 char tarefas_nome[TAREFAS_LEN][STR_MAX_LEN];
-char tarefas_data[TAREFAS_LEN][STR_MAX_LEN];
+char tarefas_descricao[TAREFAS_LEN][280];
+char tarefas_vencimento[TAREFAS_LEN][STR_MAX_LEN];
 
-void exibir_tarefas();
+int tarefas_listas[TAREFAS_LEN]; //Cria relação entre tarefas e listas
+
+// Funções principais
 void adicionar_tarefa();
 void gerenciar_tarefas();
 void gerenciar_listas();
 void filtros();
 
+// Funções auxiliares
+void tarefas_exibir();
+void listas_exibir();
+int tarefas_selecionar();
+int listas_selecionar();
+
 int main()
 {
-
     // MENU
     int op;
     do
     {
-        exibir_tarefas();
+        tarefas_exibir();
 
         // MENU DE OPÇÕES
         switch (op)
         {
-            // SAIR
+        // SAIR
         case 0:
             break;
         case 1:
-
+            gerenciar_listas();
             break;
         case 2:
-
+            gerenciar_tarefas();
             break;
         case 3:
-
+            filtros();
             break;
 
         default:
@@ -58,18 +71,52 @@ int main()
         }
 
     } while (op);
+
     return 0;
 }
 
-void exibir_tarefas()
+void tarefas_exibir()
 {
     printf("TAREFAS: \n");
-    for (int id = 0; id < top_id; id++)
+    for (int id = 0; id < tarefas_top_id; id++)
     {
         printf("%d - %s\n", id, tarefas_nome[id]);
     }
 
     return;
+}
+
+int tarefas_selecionar(){
+    int t = 0;
+
+    printf("Selecione a tarefa: \n");
+    tarefas_exibir();
+
+    scanf("%d", &t);
+
+    return t;
+}
+
+void listas_exibir()
+{
+    printf("TAREFAS: \n");
+    for (int id = 0; id < listas_top_id; id++)
+    {
+        printf("%d - %s\n", id, listas_nome[id]);
+    }
+
+    return;
+}
+
+int listas_selecionar(){
+    int l = 0;
+
+    printf("Selecione a tarefa: \n");
+    listas_exibir();
+
+    scanf("%d", &l);
+
+    return l;
 }
 
 void adicionar_tarefa()
@@ -94,11 +141,15 @@ void gerenciar_tarefas()
 
     switch (op)
     {
-    // MENU INCIAL
     case 0:
+    // MENU INICIAL
         break;
 
     case 1:
+        adicionar_tarefa();
+        break;
+    case 2:
+        
 
         break;
     default:
@@ -111,13 +162,25 @@ void gerenciar_tarefas()
 void gerenciar_listas()
 {
     int op;
+    int sel;
 
     switch (op)
     {
-    // MENU INICIAL
     case 0:
+        // MENU INICIAL
         break;
-
+    case 1:
+        //ADICIONAR LISTA
+        printf("Digite o nome da lista: ");
+        fgets(listas_nome[listas_top_id], STR_MAX_LEN, stdin);
+        listas_top_id++;        
+        break;
+    case 2:
+        //RENOMEAR LISTA
+        sel = listas_selecionar();
+        printf("Digite o nome da lista: ");
+        fgets(listas_nome[sel], STR_MAX_LEN, stdin);
+        break;
     default:
         break;
     }
@@ -131,8 +194,8 @@ void filtros()
 
     switch (op)
     {
-        // MENU INICIAL
     case 0:
+    // MENU INICIAL
         break;
 
     default:
