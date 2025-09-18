fd49274687daa5456017288c2844feab802fd5c3
Lucas25BR <lucas.santos25@ufmt.br>
2025-09-18 10:14:09 -0400

    Update gerenciador_tarefas.c

--- gerenciador_tarefas.c
+++ gerenciador_tarefas.c
@@ -1,85 +1,94 @@
+/*
+##################################################
+Alunos: Lucas Viana, João Galhard, Kevin
+Versão: 
+##################################################
+*/
+
 #include <stdio.h>
 
 #define STR_MAX_LEN 0x20
 #define TAREFAS_LEN 0x40
+#define LISTAS_LEN 0x40
 
-lista_nome[64];
+int top_id = 0;
 
-int tarefa_id[TAREFAS_LEN];
-int tarefa_prioridade[TAREFAS_LEN];
-int tarefa_lista[TAREFAS_LEN];
+listas_nome[LISTA_LEN];
 
-char tarefa_nome[TAREFAS_LEN][STR_MAX_LEN];
-char tarefa_data[TAREFAS_LEN][STR_MAX_LEN];
+int tarefas_id[TAREFAS_LEN];
+int tarefas_prioridade[TAREFAS_LEN];
+int tarefas_lista[TAREFAS_LEN];
+
+char tarefas_nome[TAREFAS_LEN][STR_MAX_LEN];
+char tarefas_data[TAREFAS_LEN][STR_MAX_LEN];
 
 
 int main() {
 
     //MENU
-    do{
+    do {
         int sel[3];
 
         //EXIBIR LISTA DE TAREFAS
-        {}
+        printf("TAREFAS: \n");
+        for (int id = 0; id < top_id; i++) {
+            printf("%d - %s\n", id, tarefas_nome[id]);
+        }
         //MENU DE OPÇÕES
         
-        switch (sel[0]){
+        switch (sel[0]) {
         //SAIR
-        case 0:
-            break;
-        case 1:
-            //GERENCIAR TAREFAS
-            switch (sel[1]) {
-            //MENU INCIAL
             case 0:
-                break;
-                
+            break;
             case 1:
-                //CRIAR NOVA TAREFA
-                switch (sel[2])
-                {
-                //MENU INICIAL
-                case 0:
-                    
+                //GERENCIAR TAREFAS
+                switch (sel[1]) {
+                    //MENU INCIAL
+                    case 0:
                     break;
+                    //CRIAR NOVA TAREFA
+                    case 1:
+                        switch (sel[2])
+                        {
+                            //MENU INICIAL
+                            case 0:
+                            break;
                     
-                default:
+                            default:
+                            break;
+                        }
+                    break;
+                    default:
+                    break;        
+                }
+            break;
+            case 2:
+                //GERENCIAR LISTAS
+                switch (sel[1]) {
+                //MENU INICIAL
+                    case 0:        
+                    break;
+
+                    default:
                     break;
                 }
-
-               break;
-            default:
-                break;        
-            }
             break;
-        case 2:
-            //GERENCIAR LISTAS
-            switch (sel[1]) {
-            //MENU INICIAL
-            case 0:        
-                break;
-
-            default:
-                break;
-            }
-            break;
-        case 3:
-            //FILTROS
-            switch (sel[1]) {
-            //MENU INICIAL
-            case 0:            
-                break;                    
+            case 3:
+                //FILTROS
+                switch (sel[1]) {
+                //MENU INICIAL
+                    case 0:            
+                    break;                    
  
-            default:
-                break;
-            }
+                    default:
+                    break;
+                }
             break;
                 
-        default:
+            default:
             break;
         }
         
     } while(sel[0]);
 
-    return 0;
-}
+return 0;
